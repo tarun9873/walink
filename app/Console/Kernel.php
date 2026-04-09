5679e210ac6a9e124fe2e5622f88c9ad933ed407
tarun <aju55355@gmail.com>
2026-04-09 20:54:35 +0530

    backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:database')->daily();
+        $schedule->command('backup:database')->dailyAt('00:00');
     }
 
     /**
