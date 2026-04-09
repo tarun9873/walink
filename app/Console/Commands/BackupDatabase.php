72e171d26ac10615186611eb062f664b16696612
tarun <aju55355@gmail.com>
2026-04-09 21:32:29 +0530

    asd

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -12,7 +12,7 @@ class BackupDatabase extends Command
   protected $description = 'Send database backup to email';
 
   public function handle()
-{
+  {
     $db = env('DB_DATABASE');
     $date = date("Y-m-d_H-i-s");
 
@@ -26,23 +26,23 @@ class BackupDatabase extends Command
     $output = '';
 
     foreach ($tables as $table) {
-        $tableName = $table->$dbName;
+      $tableName = $table->$dbName;
 
-        // Table structure
-        $createTable = \DB::select("SHOW CREATE TABLE $tableName");
-        $output .= "\n\n" . $createTable[0]->{'Create Table'} . ";\n\n";
+      // Table structure
+      $createTable = \DB::select("SHOW CREATE TABLE $tableName");
+      $output .= "\n\n" . $createTable[0]->{'Create Table'} . ";\n\n";
 
-        // Table data
-        $rows = \DB::table($tableName)->get();
+      // Table data
+      $rows = \DB::table($tableName)->get();
 
-        foreach ($rows as $row) {
-            $values = array_map(function ($value) {
-                return addslashes($value);
-            }, (array)$row);
+      foreach ($rows as $row) {
+        $values = array_map(function ($value) {
+          return addslashes($value);
+        }, (array)$row);
 
-            $values = "'" . implode("','", $values) . "'";
-            $output .= "INSERT INTO $tableName VALUES ($values);\n";
-        }
+        $values = "'" . implode("','", $values) . "'";
+        $output .= "INSERT INTO $tableName VALUES ($values);\n";
+      }
     }
 
     file_put_contents($sql_file, $output);
@@ -50,23 +50,28 @@ class BackupDatabase extends Command
     // Zip file
     $zip = new \ZipArchive();
     if ($zip->open($zip_file, \ZipArchive::CREATE) === TRUE) {
-        $zip->addFile($sql_file, basename($sql_file));
-        $zip->close();
+      $zip->addFile($sql_file, basename($sql_file));
+      $zip->close();
     }
 
     // Send Mail
     \Mail::raw('Database backup attached.', function ($message) use ($zip_file, $date) {
-        $message->to('your@email.com')
-            ->subject("Backup - $date")
-            ->attach($zip_file);
+      $message->to('your@email.com')
+        ->subject("Backup - $date")
+        ->attach($zip_file);
     });
 
     // Delete files
-    unlink($sql_file);
-    unlink($zip_file);
+    if (file_exists($sql_file)) {
+      unlink($sql_file);
+    }
+
+    if (file_exists($zip_file)) {
+      unlink($zip_file);
+    }
 
     \Log::info('Backup completed at ' . now());
 
     $this->info('Backup sent successfully!');
-}
+  }
 }
