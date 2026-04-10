45ddf13c473718c2ad9672fe0b7d64842f394c2d
tarun <aju55355@gmail.com>
2026-04-10 11:48:34 +0530

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:database')->everyTwoMinutes();
+        $schedule->command('backup:database')->dailyAt('06:00');
     }
 
 
